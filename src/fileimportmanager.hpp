da923caf1ea3fb310bf535b5c7c719532aad08da
VinTRiZz <andzig501@gmail.com>
2025-10-29 22:00:52 +0300

    (нужны тесты) менеджер импорта файлов с хешами

--- src/fileimportmanager.hpp
+++ src/fileimportmanager.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <boost/noncopyable.hpp>
-
 #include <string>
 #include <functional>
+#include <unordered_map>
+#include <vector>
+#include <chrono>
+#include <memory>
 
 namespace Common {
 
@@ -19,6 +22,15 @@ public:
         ImageFile,
     };
 
+    struct FileInfo {
+        std::string hash;
+        std::string filePath;
+        FileType type;
+        size_t size;
+        std::chrono::system_clock::time_point lastAccess;
+        std::chrono::system_clock::time_point creationTime;
+    };
+
     static FileImportManager& getInstance();
 
     void updateCache();
@@ -26,33 +38,39 @@ public:
 
     void setRootDirectory(const std::string& rDir);
     void setFileCacheSize(unsigned cacheFileCount);
+    void setMaxCacheSizeMb(unsigned maxSizeMb);
 
     void setFileTypeDetector(const std::function<FileType(const std::string&)>& ftypeDetector);
+    void setHashCalculator(const std::function<std::string(const std::string&)>& hashCalculator);
 
-    /**
-     * @brief pixmapFromPath    Считать файл
-     * @param targetPath        Путь до файла
-     * @param forceUpdate       При true принудительно обновить данные по файлу
-     * @return                  SHA-256 хэш файла
-     */
     std::string addToCache(const std::string& targetPath, bool forceUpdate = false);
-
-    /**
-     * @brief removeFromCache   Удалить файл из директории-кэша и из менеджера
-     * @param targetPath
-     */
     void removeFromCache(const std::string& targetPath) const;
 
+    bool isCached(const std::string& targetPath) const;
+    size_t getCacheSize() const;
+    size_t getCacheFileCount() const;
+    std::vector<std::string> getCachedFiles() const;
+    FileInfo getFileInfo(const std::string& targetPath) const;
+    bool validateFile(const std::string& targetPath) const;
+    void cleanupExpiredFiles();
+    void preloadDirectory(const std::string& directory);
+    std::string getFileHash(const std::string& targetPath) const;
+
 private:
-    unsigned m_fileCacheSize {10};
-    unsigned m_maxFileCacheSizeMb {10}; // TODO: Использовать
+    void cleanupCache();
+    void updateFileAccessTime(const std::string& targetPath);
+    bool copyFileToCache(const std::string& sourcePath, const std::string& hash);
+
+    unsigned m_fileCacheSize {50};
+    unsigned m_maxFileCacheSizeMb {100};
 
     std::string m_rootDirectory;
     std::function<FileType(const std::string&)> m_fileTypeDetector;
+    std::function<std::string(const std::string&)> m_hashCalculator;
 
-    std::unordered_map<std::string, std::string> m_lastUsedFiles;
-    mutable std::unordered_map<std::string, std::string> m_filePathCache;  // Словарь ПУТЬ --> ХЕШ
-    mutable std::unordered_map<std::string, std::string> m_filesCache;  // Словарь ХЕШ --> данные файла
+    mutable std::unordered_map<std::string, FileInfo> m_lastUsedFiles;
+    mutable std::unordered_map<std::string, std::string> m_filePathCache;
+    mutable std::unordered_map<std::string, std::string> m_filesCache;
 };
 
 }
